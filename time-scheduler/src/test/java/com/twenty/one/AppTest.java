c0f7d65cf350cb9482c12959305c220cd0bcf0e8
PJtheDJ403 <triet403@gmail.com>
2021-12-16 23:56:12 +0100

    added first file with maven

---
+++ time-scheduler/src/test/java/com/twenty/one/AppTest.java
@@ -0,0 +1,18 @@
+package com.twenty.one;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
