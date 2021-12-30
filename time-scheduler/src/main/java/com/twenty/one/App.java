4a671ce59dff0b5bc96e3834ed6d1ae4c19bfb7b
antemaricdev <antemaric99@gmail.com>
2021-12-30 17:30:01 +0100

    Added a GUI Folder with a basic Login Panel + admin checkbox

--- time-scheduler/src/main/java/com/twenty/one/App.java
+++ time-scheduler/src/main/java/com/twenty/one/App.java
@@ -1,9 +1,11 @@
 package com.twenty.one;
 
 
+import com.twenty.one.GUI.LoginPage;
 import com.twenty.one.database.DataBaseConnection;
 import com.twenty.one.database.DatabaseMethod;
 import com.twenty.one.database.User;
+
 
 public class App 
 {
@@ -12,5 +14,9 @@ public class App
         User user = new User("email123", "username123", "hashedPassword123",0);
         DataBaseConnection.getConnection();
         DatabaseMethod.signUp(user);
+
+        //create the object of Login Page to test if it's working
+        new LoginPage();
+
     }
 }
---
+++ time-scheduler/src/main/java/com/twenty/one/GUI/LoginPage.java
@@ -0,0 +1,34 @@
+package com.twenty.one.GUI;
+
+/**
+ * Creating a simple Login Page Panel where the user can type in his username and password and after
+ * doing so, finish with a click on the login button. The Admin checks the admin checkbox to access his admin
+ * privileges. //Prototype version 1, more functionalities and designs will be added soon
+ * @author Ante
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginPage extends JFrame{
+    private JTextField username;
+    private JPanel panel1;
+    private JPasswordField password;
+    private JButton login;
+    private JCheckBox admin;
+    private JFrame frame;
+
+
+    public LoginPage(){
+        frame = new JFrame("Login Frame");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(250, 200));
+        frame.setResizable(false);
+
+        //adding the panel
+        frame.add(panel1);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
