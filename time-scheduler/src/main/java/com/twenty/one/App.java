38b6ff4adfe073d7264e960dc559e1ca2c3d4853
PJ <triet403@gmail.com>
2021-12-31 00:12:48 +0100

    merge tree back

--- time-scheduler/src/main/java/com/twenty/one/App.java
+++ time-scheduler/src/main/java/com/twenty/one/App.java
@@ -1,18 +1,17 @@
 package com.twenty.one;
 
 
-import com.twenty.one.GUI.LoginPage;
-import com.twenty.one.database.DataBaseConnection;
-import com.twenty.one.database.DatabaseMethod;
-import com.twenty.one.database.User;
+import com.twenty.one.mailAPI.mailUtils;
 
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception {
         //create the object of Login Page to test if it's working
-        new LoginPage();
+        //new LoginPage();
+
+        //Sending an email to recipient
+        mailUtils.sendMail("javacomtwentyone@gmail.com");
 
     }
 }
---
+++ time-scheduler/src/main/java/com/twenty/one/mailAPI/mailUtils.java
@@ -0,0 +1,58 @@
+package com.twenty.one.mailAPI;
+
+/**
+ * simple program to establish a connection with an email provider (in this case gmail) and
+ * send an email to that adress
+ *
+ * I will comment it out as soon as I can so you guys can understand what's happening
+ *
+ * @author Ante
+ */
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class mailUtils {
+    public static void sendMail(String recipient) throws Exception {
+        System.out.println("Preparing to send email");
+        Properties properties = new Properties(); //assign key and value to property
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        String emailAddress = "javacomtwentyone@gmail.com";
+        String password = "StrongPassword21";
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(emailAddress, password);
+                    }
+                });
+
+        Message message = prepareMessage(session, emailAddress, recipient);
+
+        Transport.send(message);
+        System.out.println("Message sent SUCCESSFULLY");
+    }
+
+    private static Message prepareMessage(Session session, String emailAddress, String recipient) {
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(emailAddress));
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            message.setSubject("THIS CODE WORKS WOHOOOO");
+            message.setText("Hey There, \n This is a reminder email for the upcoming event");
+            return message;
+        } catch (Exception e) {
+            Logger.getLogger(mailUtils.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return null;
+    }
+}
