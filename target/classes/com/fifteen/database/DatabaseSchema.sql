8a25491f922bf5829a4823379b9b426a37087757
Tim <goerss@stud.fra-uas.de>
2022-01-26 15:26:53 +0100

    added jdatechooser

--- target/classes/com/fifteen/database/DatabaseSchema.sql
+++
@@ -1,9 +0,0 @@
- drop table userdb;
- 
- create table UserDB (
-    email    varchar2(100) not null unique,
-    username varchar2(100) not null,
-    hashed_password varchar2(64) not null,
-    user_id raw(16) default sys_guid() constraint userdb_userid_pk primary key,
-    is_admin number(1) not null
-)
