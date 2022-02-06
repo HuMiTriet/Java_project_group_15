0c86999ad4bb6cf810bd1e6f0e334ae956339c3b
PJ <triet403@gmail.com>
2022-02-06 22:03:09 +0100

    added some javadoc comment

--- src/main/java/com/fifteen/auth/security/PasswordHasher.java
+++ src/main/java/com/fifteen/auth/security/PasswordHasher.java
@@ -3,14 +3,18 @@ package com.fifteen.auth.security;
 import org.apache.commons.codec.digest.DigestUtils;
 
 /**
- * simple hash function to hash any string into a string of 64 character
+ * simple hash function to hash any string into a string of 64 character.
+ * This is the hashed password that will finally be stored in the remote
+ * database.
+ * 
  * @author Triet Huynh
  * @see https://www.baeldung.com/sha-256-hashing-java
  */
 public class PasswordHasher {
-	private PasswordHasher(){}
+  private PasswordHasher() {
+  }
 
-	public static String sha2(String password) {
-		return DigestUtils.sha256Hex(password);
-	}
+  public static String sha2(String password) {
+    return DigestUtils.sha256Hex(password);
+  }
 }
--- src/main/java/com/fifteen/auth/security/UserAuthenticator.java
+++ src/main/java/com/fifteen/auth/security/UserAuthenticator.java
@@ -7,10 +7,28 @@ import javax.swing.JOptionPane;
 
 import com.fifteen.database.DBMethod;
 
+/**
+ * Provide a suite of method to authenticate user. This is used in the login
+ * page and the sign up page and deal mostly with the remote database.
+ * 
+ * @author Triet Huynh
+ */
 public class UserAuthenticator {
   private UserAuthenticator() {
   };
 
+  /**
+   * Authenticate user, check the remote data base and see if the user with that
+   * email has the same hashed password created locally as the hashed password
+   * in the remote database.
+   * 
+   * @param username
+   *                 username of user
+   * @param password
+   *                 password of user
+   * @return true if user is authenticated, false otherwise
+   * @author Triet Huynh
+   */
   private static boolean authenticate(String email, String enteredPassword) throws SQLException {
 
     boolean result = false;
@@ -22,6 +40,13 @@ public class UserAuthenticator {
     return result;
   }
 
+  /**
+   * Helpter function to throw an error panel if there is error related to
+   * connection with the remote database.
+   * 
+   * @param e takes in the SQL exception error
+   * @author Triet Huynh
+   */
   private static void throwErrorpane(SQLException e) {
     JOptionPane.showMessageDialog(null, "Failed connecting to database,"
         + " Please check your internet connection");
@@ -30,6 +55,11 @@ public class UserAuthenticator {
     e.printStackTrace();
   }
 
+  /**
+   * @return false if the email is not filled or filled with spaces, true if
+   *         otherwise and the email is in the correct format
+   * @author Triet Huynh
+   */
   public static boolean checkEmailFormat(JLabel emailLabel, String enteredEmail) {
 
     if (enteredEmail.isBlank()) {
@@ -47,6 +77,18 @@ public class UserAuthenticator {
 
   }
 
+  /**
+   * This function is used to check whether the entered email exist on the remote
+   * database. If the email does not exist, the email label will be set to error
+   * messeage specify in the String message
+   * 
+   * @param emailLabel   the label that will show the error
+   * @param enteredEmail the email that the user entered
+   * @param message      the error message that will be shown if the email does
+   *                     not exist
+   * 
+   * @author Triet Huynh
+   */
   public static boolean authenticateEmailField(JLabel emailLabel,
       String enteredEmail, String message) {
 
