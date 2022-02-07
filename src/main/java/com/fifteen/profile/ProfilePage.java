9b578e0ff66ad99ae5e24f480b2928a91476f2dc
Jorge <jorvanari@gmail.co>
2022-02-07 14:19:16 +0100

    In ProfilePage added ID

--- src/main/java/com/fifteen/profile/ProfilePage.java
+++ src/main/java/com/fifteen/profile/ProfilePage.java
@@ -1,6 +1,7 @@
 package com.fifteen.profile;
 
 import com.fifteen.database.User;
+import com.fifteen.events.settings.EventSettings;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
@@ -18,13 +19,14 @@ import java.util.Locale;
 public class ProfilePage extends JFrame {
     private JFrame frame;
     private JPanel panel1;
-    private JLabel username;
+    private JLabel profileUsername;
     private JPanel conTitle;
     private JButton editContact;
-    private JLabel jLabelemail;
+    private JLabel profileEmail;
     private JLabel welcomLabel;
     private JLabel cpdLabel;
     private JButton settingsButton;
+    private JLabel profileID;
 
 
     public ProfilePage(User user) {
@@ -45,12 +47,21 @@ public class ProfilePage extends JFrame {
             }
         });
 
-        username.setText(user.getUsername());
-        jLabelemail.setText(user.getEmail());
+        profileUsername.setText(user.getUsername());
+        profileEmail.setText(user.getEmail());
+        profileID.setText(user.getUserID());
+
+
         settingsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        settingsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new EventSettings();
             }
         });
     }
@@ -74,31 +85,31 @@ public class ProfilePage extends JFrame {
         panel1 = new JPanel();
         panel1.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
         conTitle = new JPanel();
-        conTitle.setLayout(new GridLayoutManager(11, 2, new Insets(0, 0, 0, 0), -1, -1));
+        conTitle.setLayout(new GridLayoutManager(12, 2, new Insets(0, 0, 0, 0), -1, -1));
         panel1.add(conTitle, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-        username = new JLabel();
-        Font usernameFont = this.$$$getFont$$$("Fira Code", Font.BOLD, 20, username.getFont());
-        if (usernameFont != null) username.setFont(usernameFont);
-        username.setText("");
-        conTitle.add(username, new GridConstraints(2, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        jLabelemail = new JLabel();
-        jLabelemail.setEnabled(false);
-        Font jLabelemailFont = this.$$$getFont$$$("Fira Code", Font.PLAIN, 14, jLabelemail.getFont());
-        if (jLabelemailFont != null) jLabelemail.setFont(jLabelemailFont);
-        jLabelemail.setText("");
-        conTitle.add(jLabelemail, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        profileUsername = new JLabel();
+        Font profileUsernameFont = this.$$$getFont$$$("Fira Code", Font.BOLD, 20, profileUsername.getFont());
+        if (profileUsernameFont != null) profileUsername.setFont(profileUsernameFont);
+        profileUsername.setText("");
+        conTitle.add(profileUsername, new GridConstraints(2, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        profileEmail = new JLabel();
+        profileEmail.setEnabled(false);
+        Font profileEmailFont = this.$$$getFont$$$("Fira Code", Font.PLAIN, 14, profileEmail.getFont());
+        if (profileEmailFont != null) profileEmail.setFont(profileEmailFont);
+        profileEmail.setText("");
+        conTitle.add(profileEmail, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
         conTitle.add(spacer1, new GridConstraints(1, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         final JLabel label1 = new JLabel();
         label1.setText("Contacts");
-        conTitle.add(label1, new GridConstraints(8, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        conTitle.add(label1, new GridConstraints(9, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer2 = new Spacer();
-        conTitle.add(spacer2, new GridConstraints(7, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        conTitle.add(spacer2, new GridConstraints(8, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         editContact = new JButton();
         editContact.setText("Edit Contacts");
-        conTitle.add(editContact, new GridConstraints(9, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        conTitle.add(editContact, new GridConstraints(10, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer3 = new Spacer();
-        conTitle.add(spacer3, new GridConstraints(10, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        conTitle.add(spacer3, new GridConstraints(11, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         welcomLabel = new JLabel();
         Font welcomLabelFont = this.$$$getFont$$$("Fira Code", Font.ITALIC, 12, welcomLabel.getFont());
         if (welcomLabelFont != null) welcomLabel.setFont(welcomLabelFont);
@@ -108,10 +119,15 @@ public class ProfilePage extends JFrame {
         conTitle.add(spacer4, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         cpdLabel = new JLabel();
         cpdLabel.setText("Change Profile Details");
-        conTitle.add(cpdLabel, new GridConstraints(5, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        conTitle.add(cpdLabel, new GridConstraints(6, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         settingsButton = new JButton();
         settingsButton.setText("Settings");
-        conTitle.add(settingsButton, new GridConstraints(6, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        conTitle.add(settingsButton, new GridConstraints(7, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        profileID = new JLabel();
+        Font profileIDFont = this.$$$getFont$$$("Fira Code", Font.PLAIN, 10, profileID.getFont());
+        if (profileIDFont != null) profileID.setFont(profileIDFont);
+        profileID.setText("");
+        conTitle.add(profileID, new GridConstraints(5, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 
     /**
