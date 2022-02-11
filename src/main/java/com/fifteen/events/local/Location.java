c746a528e7aba0412f01b0fa456a55949aff412b
PJ <triet403@gmail.com>
2022-02-11 22:04:35 +0100

    remove the location class calculate distance method

--- src/main/java/com/fifteen/events/local/Location.java
+++ src/main/java/com/fifteen/events/local/Location.java
@@ -7,11 +7,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 /**
- * Class to store the location and a function to calculate the distance between
- * two Location class. This class is taken from Princeton University's cs class
- * please check the link to the orginal code.
- * 
- * @see https://introcs.cs.princeton.edu/java/44st/Location.java.html
+ * Class to store the location of an event.
  * 
  * @author Triet Huynh
  */
@@ -24,21 +20,4 @@ public class Location {
   private String name = "NA";
   private double longitude = 0.0;
   private double latitude = 0.0;
-
-  public double distanceTo(Location that) {
-    double KILOMETER_PER_NAUTICAL_MILE = 1.852;
-    double lat1 = Math.toRadians(this.latitude);
-    double lon1 = Math.toRadians(this.longitude);
-    double lat2 = Math.toRadians(that.latitude);
-    double lon2 = Math.toRadians(that.longitude);
-
-    // great circle distance in radians, using law of cosines formula
-    double angle = Math.acos(Math.sin(lat1) * Math.sin(lat2)
-        + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
-
-    // each degree on a great circle of Earth is 60 nautical miles
-    double nauticalMiles = 60 * Math.toDegrees(angle);
-    double kilometers = KILOMETER_PER_NAUTICAL_MILE * nauticalMiles;
-    return kilometers;
-  }
 }
--- src/main/java/com/fifteen/events/sync/reAuthenticatePage.java
+++ src/main/java/com/fifteen/events/sync/reAuthenticatePage.java
@@ -8,17 +8,30 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class reAuthenticatePage {
-  private JTextField textField1;
-  private JTextField textField2;
+public class reAuthenticatePage extends JFrame {
+  private JTextField emailTextField;
+  private JTextField passwordTextField;
   private JButton submitButton;
   private JLabel displayMessage;
   private JLabel displayEmail;
   private JLabel emailJlabel;
   private JLabel passwordJlabel;
   private JLabel displayPassword;
+  private JPanel reAuthPanel;
+  private JFrame frame;
 
   public reAuthenticatePage() {
+
+    frame = new JFrame("Re-Authenticate");
+
+    frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    frame.setPreferredSize(new Dimension(300, 300));
+    frame.setResizable(false);
+
+    frame.add(reAuthPanel);
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
     submitButton.addActionListener(new ActionListener() {
       /**
        * Invoked when an action occurs.
@@ -47,45 +60,61 @@ public class reAuthenticatePage {
    * @noinspection ALL
    */
   private void $$$setupUI$$$() {
-    final JPanel panel1 = new JPanel();
-    panel1.setLayout(new GridLayoutManager(8, 1, new Insets(0, 0, 0, 0), -1, -1));
-    textField1 = new JTextField();
-    panel1.add(textField1,
+    reAuthPanel = new JPanel();
+    reAuthPanel.setLayout(new GridLayoutManager(8, 1, new Insets(0, 0, 0, 0), -1, -1));
+    emailTextField = new JTextField();
+    reAuthPanel.add(emailTextField,
         new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL,
             GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null,
             0, false));
-    textField2 = new JTextField();
-    panel1.add(textField2,
+    passwordTextField = new JTextField();
+    reAuthPanel.add(passwordTextField,
         new GridConstraints(5, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL,
             GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null,
             0, false));
     submitButton = new JButton();
     submitButton.setText("Submit");
-    panel1.add(submitButton,
+    reAuthPanel.add(submitButton,
         new GridConstraints(7, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL,
             GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW,
             GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     emailJlabel = new JLabel();
     emailJlabel.setText("");
-    panel1.add(emailJlabel, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
-        GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    reAuthPanel.add(emailJlabel,
+        new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     passwordJlabel = new JLabel();
     passwordJlabel.setText("");
-    panel1.add(passwordJlabel, new GridConstraints(6, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
-        GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    reAuthPanel.add(passwordJlabel,
+        new GridConstraints(6, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     displayMessage = new JLabel();
     displayMessage.setHorizontalAlignment(0);
     displayMessage.setHorizontalTextPosition(0);
     displayMessage.setText("Please enter your email and password to sync your work");
-    panel1.add(displayMessage, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
-        GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    reAuthPanel.add(displayMessage,
+        new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     displayEmail = new JLabel();
     displayEmail.setText("Email");
-    panel1.add(displayEmail, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
-        GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    reAuthPanel.add(displayEmail,
+        new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     displayPassword = new JLabel();
     displayPassword.setText("Password");
-    panel1.add(displayPassword, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
-        GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+    reAuthPanel.add(displayPassword,
+        new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE,
+            GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+  }
+
+  /**
+   * @noinspection ALL
+   */
+  public JComponent $$$getRootComponent$$$() {
+    return reAuthPanel;
+  }
+
+  private void createUIComponents() {
+    // TODO: place custom component creation code here
   }
 }
