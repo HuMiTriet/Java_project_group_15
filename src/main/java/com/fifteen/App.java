b0ca0ec16d376a6183f42c12a4c278393acd7430
Ante Maric <49611582+anmaricdev@users.noreply.github.com>
2022-02-12 17:17:40 +0100

    task and timer test

--- src/main/java/com/fifteen/App.java
+++ src/main/java/com/fifteen/App.java
@@ -10,6 +10,8 @@ import com.formdev.flatlaf.FlatIntelliJLaf;
 
 import org.apache.commons.io.FileUtils;
 
+import static com.fifteen.events.reminder.sendReminders.reminder;
+
 public class App {
   public static void main(String[] args) throws Exception {
     // create the object of Login Page to test if it's working
@@ -24,14 +26,15 @@ public class App {
     // Added functionality to the Login page
     // Available user: email: t@g.com | password: t
     // Available admin: email: f@g.com | password: f
-    DBMethod.closeConnection();
-    new LoginPage();
+    // DBMethod.closeConnection();
+    // new LoginPage();
 
     // UserDao userHandler = new UserDaoImp();
     // User user = userHandler.createUserFromLogin("t@g.com");
     // new CalendarView(user);
 
     // new AdminPage();
+    reminder();
 
     // mailUtils.sendMail("javacomtwentyone@gmail.com");
   }
--- src/main/java/com/fifteen/events/reminder/sendReminders.java
+++ src/main/java/com/fifteen/events/reminder/sendReminders.java
@@ -11,25 +11,29 @@ import java.util.TimerTask;
  */
 
 public class sendReminders {
-    public static int convert(String option)
+    public static void reminder()
     {
         Timer timer = new Timer(); //timer calculates time to execute a task
 
         //When the timer is up, run is going to be executed inside our TimerTask instance
         TimerTask task = new TimerTask() {
             @Override
-            public void run() {
+            public void run() { //run is an abstract method
                 System.out.println("Task is complete!");
             }
         };
 
         Calendar date = Calendar.getInstance();
         date.set(Calendar.YEAR,2022);
-        date.set(Calendar.MONTH,Calendar.JANUARY);
-        date.set(Calendar.DAY_OF_MONTH,10);
-        //timer.schedule(task, 1000);
+        date.set(Calendar.MONTH,Calendar.FEBRUARY);
+        date.set(Calendar.DAY_OF_MONTH,12);
+        date.set(Calendar.HOUR_OF_DAY,16);
+        date.set(Calendar.MINUTE,55);
+        date.set(Calendar.SECOND,20);
+        date.set(Calendar.MILLISECOND,0);
 
-        int minute = 0;
-        return minute;
+        //timer.schedule(task, 1000);
+        timer.schedule(task, date.getTime());
+
     }
 }
